606c0d218168d5e582f5d3bca56da16081de7698
david weil <david.weil@coinfabrik.com>
2025-01-15 11:11:25 -0300

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,40 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+//const LockModule = require("../ignition/modules/Lock");
+//const { lock } = await hre.ignition.deploy(ApolloModule);
+//console.log(`Apollo deployed to: ${await lock.getAddress()}`);
+
+
+task("query", "Query state value: parameters: address")
+	.addPositionalParam("address", "The contract address")
+	.setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory("Lock");
+    const contract = MyContract.attach(taskArgs.address);
+    console.log("getlen:", await contract.getlen());
+});
+
+task("reset", "Reset state value")
+	.addPositionalParam("address", "The contract address")
+	.setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory("Lock");
+    const contract = MyContract.attach(taskArgs.address);
+    const tx = await contract.reset();
+    console.log("reset:", tx);
+});
+
+task("set", "Set state value")
+	.addPositionalParam("address", "The contract address")
+	.setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory("Lock");
+    const contract = MyContract.attach(taskArgs.address);
+    const tx = await contract.push(42);
+    console.log("set:", tx);
+});
+
+
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
