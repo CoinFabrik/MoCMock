cdefea6f02d20bb9baceab527f24d2790835ef39
Julian Zamt <julian.zamt@coinfabrik.com>
2025-01-17 16:49:14 -0300

    change test name

--- test/MoCMock.ts
+++ test/MoCMock.ts
@@ -10,31 +10,31 @@ describe('MoCMock', function () {
   async function deployOneYearMoCMockFixture() {
     const [owner, otherAccount] = await hre.ethers.getSigners();
     const MoCMock = await hre.ethers.getContractFactory('MoCMock');
-    const lock = await MoCMock.deploy();
-    return { lock, owner, otherAccount };
+    const mocMock = await MoCMock.deploy();
+    return { mocMock, owner, otherAccount };
   }
 
   describe('Deployment', function () {
     it('Should init with zero', async function () {
-      const { lock } = await loadFixture(deployOneYearMoCMockFixture);
-      expect(await lock.mylen()).to.equal(0);
-      const _len = await lock.getlen();
+      const { mocMock } = await loadFixture(deployOneYearMoCMockFixture);
+      expect(await mocMock.mylen()).to.equal(0);
+      const _len = await mocMock.getlen();
       expect(_len).to.equal(0);
     });
 
     it('Push/pop/reset works..', async function () {
-      const { lock, owner } = await loadFixture(deployOneYearMoCMockFixture);
-      expect(await lock.getlen()).to.equal(0);
-      await lock.push(4);
-      expect(await lock.getlen()).to.equal(1);
-      await lock.push(4);
-      expect(await lock.getlen()).to.equal(2);
-      await lock.push(5);
-      expect(await lock.getlen()).to.equal(3);
-      await lock.pop();
-      expect(await lock.getlen()).to.equal(2);
-      await lock.reset();
-      expect(await lock.getlen()).to.equal(0);
+      const { mocMock, owner } = await loadFixture(deployOneYearMoCMockFixture);
+      expect(await mocMock.getlen()).to.equal(0);
+      await mocMock.push(4);
+      expect(await mocMock.getlen()).to.equal(1);
+      await mocMock.push(4);
+      expect(await mocMock.getlen()).to.equal(2);
+      await mocMock.push(5);
+      expect(await mocMock.getlen()).to.equal(3);
+      await mocMock.pop();
+      expect(await mocMock.getlen()).to.equal(2);
+      await mocMock.reset();
+      expect(await mocMock.getlen()).to.equal(0);
     });
   });
 });
