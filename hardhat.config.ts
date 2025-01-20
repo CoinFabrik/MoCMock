94182fae3491c3d99f9d195d3b65e5a3d7e4c389
Julian Zamt <julian.zamt@coinfabrik.com>
2025-01-20 11:51:16 -0300

    add by fn set/read tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,4 @@
-import { HardhatUserConfig } from 'hardhat/config';
+import { HardhatUserConfig, task } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 //const MoCMockModule = require("../ignition/modules/MoCMock");
@@ -31,6 +31,123 @@ task('set', 'Set state value')
     console.log('set:', tx);
   });
 
+// **********************
+// Set to arbitrary value
+// **********************
+task('setQAClock', 'Set qACLockedInPending var to an arbitrary value')
+  .addPositionalParam('address', 'The contract address')
+  .addPositionalParam('val', 'qACLockedInPending desired value')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const tx = await contract.setQACLockedInPending(taskArgs.val);
+      console.log('tx:', tx);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+task('setEmaBool', 'Set emaBool var -  0 (false) : 1 (true)')
+  .addPositionalParam('address', 'The contract address')
+  .addPositionalParam('val', '0 (false) : 1 (true)')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const tx = await contract.setEmaBool(Number(taskArgs.val));
+      console.log('tx:', tx);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+task('setBts', 'Set bts var to an arbitrary value')
+  .addPositionalParam('address', 'The contract address')
+  .addPositionalParam('val', 'bts desired value')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const tx = await contract.setBts(taskArgs.val);
+      console.log('tx:', tx);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+task('setNextTC', 'Set nextTCInterestPayment var to an arbitrary value')
+  .addPositionalParam('address', 'The contract address')
+  .addPositionalParam('val', 'nextTCInterestPayment desired value')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const tx = await contract.setNextTCInterestPayment(taskArgs.val);
+      console.log('tx:', tx);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+// **************
+// Query by value
+// **************
+task('readQAClock', 'Query qACLockedInPending var')
+  .addPositionalParam('address', 'The contract address')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const qACLockedInPending = await contract.qACLockedInPending();
+      console.log('qACLockedInPending:', qACLockedInPending);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+task('readEmaBool', 'Query emaBool var')
+  .addPositionalParam('address', 'The contract address')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const emaBool = await contract.emaBool();
+      console.log('emaBool:', emaBool);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+task('readBts', 'Query bts var')
+  .addPositionalParam('address', 'The contract address')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const tx = await contract.getBts();
+      console.log('bts:', tx);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+task('readNextTC', 'Query nextTCInterestPayment var')
+  .addPositionalParam('address', 'The contract address')
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const MyContract = await ethers.getContractFactory('MoCMock');
+    const contract = MyContract.attach(taskArgs.address);
+    try {
+      const tx = await contract.nextTCInterestPayment();
+      console.log('nextTCInterestPayment:', tx);
+    } catch (e) {
+      throw new Error(`Unexpected error: ${e}`);
+    }
+  });
+
+// ****************
+// Hardhat config
+// ****************
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
   networks: {
